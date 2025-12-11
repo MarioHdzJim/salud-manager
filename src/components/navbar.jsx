a18a81ea97a8c12462ef0d782e6fd9c62535ce61
unknown <altamiragobiernodrive@gmail.com>
2025-12-10 18:58:32 -0600

    Cambios realizados en el navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,22 +3,35 @@ import { Link } from 'react-router-dom';
 import './navbar.css';
 
 const navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar__container">
         <h2 className="navbar__logo">SaludManager</h2>
-        
-        <ul className="navbar__menu">
+        <div className="navbar__toggle" onClick={toggleMenu}>
+          {isOpen ? '✖' : '☰'}
+        </div>
+
+        <ul className={`navbar__menu ${isOpen ? 'active' : ''}`}>
           <li className="navbar__item">
-            <Link to="/" className="navbar__link">Dashboard</Link>
+            <Link to="/" className="navbar__link" onClick={closeMenu}>Dashboard</Link>
           </li>
           
           <li className="navbar__item">
-            <Link to="/pacientes" className="navbar__link">Pacientes</Link>
+            <Link to="/pacientes" className="navbar__link" onClick={closeMenu}>Pacientes</Link>
           </li>
           
           <li className="navbar__item">
-            <Link to="/profile" className="navbar__link">Mi Perfil</Link>
+            <Link to="/perfil" className="navbar__link" onClick={closeMenu}>Mi Perfil</Link>
           </li>
         </ul>
       </div>
